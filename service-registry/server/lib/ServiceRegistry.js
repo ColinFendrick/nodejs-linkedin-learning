08f86baa4779b5523e093a415fd13ac1d2fd01d7
Colin Fendrick <colin.fendrick@gmail.com>
2022-10-13 13:18:47 -0700

    Add register/unregister to speaker service

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -11,10 +11,12 @@ class ServiceRegistry {
     this.cleanup();
     const candidates = Object.values(this.services)
       .filter(service => service.name === name && semver.satisfies(service.version, version));
+
     return candidates[Math.floor(Math.random() * candidates.length)];
   }
 
   register(name, version, ip, port) {
+    this.cleanup();
     const key = name + version + ip + port;
 
     if (!this.services[key]) {
@@ -27,7 +29,6 @@ class ServiceRegistry {
       this.log.debug(`Added services ${name}, version ${version} at ${ip}:${port}`);
       return key;
     }
-
     this.services[key].timestamp = Math.floor(new Date() / 1000);
     this.log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`);
     return key;
@@ -36,6 +37,7 @@ class ServiceRegistry {
   unregister(name, version, ip, port) {
     const key = name + version + ip + port;
     delete this.services[key];
+    this.log.debug(`Unregistered services ${name}, version ${version} at ${ip}:${port}`);
     return key;
   }
 
