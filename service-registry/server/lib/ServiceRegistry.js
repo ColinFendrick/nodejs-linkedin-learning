134acff67992b5b6d79ceb4404396dcdf9148fb0
Colin Fendrick <colin.fendrick@gmail.com>
2022-10-13 11:39:59 -0700

    Expire services

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -8,9 +8,9 @@ class ServiceRegistry {
   }
 
   get(name, version) {
+    this.cleanup();
     const candidates = Object.values(this.services)
       .filter(service => service.name === name && semver.satisfies(service.version, version));
-    
     return candidates[Math.floor(Math.random() * candidates.length)];
   }
 
@@ -37,6 +37,16 @@ class ServiceRegistry {
     const key = name + version + ip + port;
     delete this.services[key];
     return key;
+  }
+
+  cleanup() {
+    const now = Math.floor(new Date() / 1000);
+    Object.keys(this.services).forEach((key) => {
+      if (this.services[key].timestamp + this.timeout < now) {
+        delete this.services[key];
+        this.log.debug(`Removed service ${key}`);
+      }
+    });
   }
 }
 
